9f2c03340f284ea5c9d2b231f2b976dc87f58562
Koerismo <56563658+koerismo@users.noreply.github.com>
2021-10-08 14:42:22 -0400

    Update basicWindow.py

--- examples/basicWindow.py
+++ examples/basicWindow.py
@@ -1,7 +1,7 @@
 import bgfx
 
 # Create a window
-myWin = bgfx.Window( 'BGFX Demonstration', width=350, height=200, bg='#111' )
+myWin = bgfx.Window( 'BGFX Demonstration', width=350, height=200, bg='white' )
 
 # Start window loop
 while myWin.isOpen: myWin.update()
