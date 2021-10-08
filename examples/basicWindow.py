873678538b8f39b56bdbfef2dabd3445388cb42c
Koerismo <56563658+koerismo@users.noreply.github.com>
2021-10-08 14:29:25 -0400

    Update basicWindow.py

--- examples/basicWindow.py
+++ examples/basicWindow.py
@@ -1,8 +1,10 @@
+import bgfx
+
 # Create a window
-myWin	= Window( 'BGFX Demonstration', width=350, height=200, bg='#111' )
+myWin = bgfx.Window( 'BGFX Demonstration', width=350, height=200, bg='#111' )
 
 # Create a text object
-myTitle	= Text( myWin, 175, 100, 'Hello, world!', fill='#999', fontSize=30 )
+myTitle = bgfx.Text( myWin, 175, 100, 'Hello, world!', fill='#999', fontSize=30 )
 
 # Start window loop
 while myWin.isOpen:
