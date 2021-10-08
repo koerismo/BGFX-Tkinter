fe6caf01c3690f9f53b90bf978c795ba4ee5fd6a
Koerismo <56563658+koerismo@users.noreply.github.com>
2021-10-08 15:08:46 -0400

    Create mouseInputEvents.py

---
+++ examples/mouseInputEvents.py
@@ -0,0 +1,27 @@
+import bgfx
+
+# Create a window and disable the user's cursor when hovering over the window
+myWin = bgfx.Window( 'Mouse Input Events', width=300, height=300, bg='black', cursor=False )
+
+# Create a square to act as a cursor
+myCursor = bgfx.Rect( myWin, 0, 0, 10, 10, fill='', outline='white' )
+
+# Create new events for mouse actions.
+def onMove( mouseX, mouseY ):
+	myCursor.x = mouseX - 5
+	myCursor.y = mouseY - 5
+
+def onPress( mouseX, mouseY ):
+	myCursor.fill = 'white'
+
+def onRelease( mouseX, mouseY ):
+	myCursor.fill = ''
+
+# Register the events with the window
+myWin.onMouseMove = onMove
+myWin.onMousePressed = onPress
+myWin.onMouseReleased = onRelease
+
+# Start the main loop
+while myWin.isOpen:
+	myWin.update()
