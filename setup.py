f2b908bf0b897fd70de5dd75ca0b8fc96a6a4775
Koerismo <56563658+koerismo@users.noreply.github.com>
2021-10-13 20:30:34 -0400

    Update version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
 	name='bgfx',
-	version='0.7.0',
+	version='0.7.5',
 	description='A clean wrapper for tkinter canvas applications.',
 	url='https://github.com/koerismo/BGFX-Tkinter/tree/pip',
 	author='Koerismo',
